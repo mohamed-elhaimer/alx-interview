f91e788fe0712da69f288b9a18402c7829bb6d8c
Logic <mohamedelheimer0@gmail.com>
2024-07-11 17:14:06 +0100

    all

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
-"""
-The coin change problem
-"""
-from typing import List
+
+""" Contains makeChange function"""
 
 
-def makeChange(coins: List[int], total: int) -> int:
+def makeChange(coins, total):
     """
-    makeChange Function
+    Returns: fewest number of coins needed to meet total
+        If total is 0 or less, return 0
+        If total cannot be met by any number of coins you have, return -1
     """
-
+    if not coins or coins is None:
+        return -1
     if total <= 0:
         return 0
-
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
+    change = 0
+    coins = sorted(coins)[::-1]
     for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-
-    return dp[total] if dp[total] != float('inf') else -1
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
